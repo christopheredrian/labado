2b5b5680949d435f4a2a6c5327f8be34485ae40c
Christopher Espiritu <christopheredrian@gmail.com>
2018-03-07 23:36:43 +0800

    Added entrust

--- config/app.php
+++ config/app.php
@@ -160,6 +160,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /* Added for LabaDo */
+        Zizaco\Entrust\EntrustServiceProvider::class,
+
     ],
 
     /*
@@ -208,6 +211,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /* Added for LabaDo */
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 
--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,17 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => App\User::class,
+            'table' => 'users'
         ],
+
+        // From Entrust
+//        'providers' => [
+//            'users' => [
+//                'driver' => 'eloquent',
+//                'model' => Namespace\Of\Your\User\Model\User::class,
+//                'table' => 'users',
+//            ],
+//        ],
 
         // 'users' => [
         //     'driver' => 'database',
