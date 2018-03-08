20ee813140619dab9d330f6fd30b074ae3830618
Jay Garcia <2151796@slu.edu.ph>
2018-03-08 21:01:58 +0800

    added migrations for transactions and customers

---
+++ database/migrations/2018_03_08_125050_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('status',['request','accepted','declined','paid']);
+            $table->double('amount');
+            $table->dateTime('date_requested');
+            $table->dateTime('date_pickedup');
+            $table->dateTime('date_delivered');
+            $table->dateTime('date_paid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
